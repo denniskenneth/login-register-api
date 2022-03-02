510faaa0846cc2b2072af7817e16d50353597fb0
Dennis Kenneth <dennyify123@gmail.com>
2022-03-02 23:01:38 +0000

    user login controller functionality added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,4 +7,28 @@ exports.register = (req, res, next) => {
   const salt = bcryptjs.genSalt(10);
 
   req.body.password = bcryptjs.hashSync(password, salt);
+
+  userService.register(req.body, (error, result) => {
+    if (error) {
+      return next(error);
+    }
+    return res.status(200).send({
+      message: 'Success',
+      data: result,
+    });
+  });
+};
+
+exports.login = (req, res, next) => {
+  const { username, password } = req.body;
+
+  userService.login({ username, password }, (error, result) => {
+    if (error) {
+      return next(error);
+    }
+    return res.status(200).send({
+      message: 'Success',
+      data: result,
+    });
+  });
 };
